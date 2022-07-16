047c9dbdda5c90d1aee5e51411e97e62d85e8e86
Carlos Gonzalez <siscond@hotmail.com>
2022-07-15 20:52:15 -0400

    Alert color

--- resources/views/quiz/results.blade.php
+++ resources/views/quiz/results.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <div class="col-12 pt-2">
-            {{$user[0]->name}}
+        <div class="alert alert-primary" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Aww yeah, you successfully completed this quiz.</p>
+            <hr>
         </div>
         <div class="col-12 pt-2">
-            {{$quiz[0]->name}} : {{$quiz[0]->description}}
+            <div class="card">
+                <div class="card-body">
+                    {{$user[0]->name}}
+                </div>
+            </div>           
+        </div>
+        <div class="col-12 pt-2">
+            <div class="card">
+                <div class="card-body">
+                    {{$quiz[0]->name}} : {{$quiz[0]->description}}
+                </div>
+            </div> 
         </div>        
         <ol>
         @foreach( $questions as $index => $q)
@@ -14,7 +27,7 @@
                 @if( count($q['response']) > 0)
                 <ul>
                     @if($q["response"]["value"] == 0 )
-                    <li class="alert alert-warning">{{$q["response"]["option"]}}</li>
+                    <li class="alert alert-danger">{{$q["response"]["option"]}}</li>
                     @else
                     <li class="alert alert-success">{{$q["response"]["option"]}}</li>
                     @endif
