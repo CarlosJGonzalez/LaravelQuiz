1144baf26acfe40d8737229f7a6053d76a0d72cc
Carlos Gonzalez <siscond@hotmail.com>
2022-07-17 13:49:55 -0400

    Pull live quizzes

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -24,7 +24,7 @@ class QuizController extends Controller
     public function index()
     {
         try{
-            $quizzes = Quiz::all();
+            $quizzes = Quiz::all()->where('live', 1);
             if(!$quizzes){
                 return view('404');
             }
